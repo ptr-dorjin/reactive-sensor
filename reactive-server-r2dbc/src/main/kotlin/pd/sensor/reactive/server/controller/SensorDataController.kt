1a9b4be9215e18ccc3d822e06a3c437ed988ba00
Petr Dorzhinov <dorzhinov@gmail.com>
2021-07-21 00:40:30 -0700

    updated docs

--- reactive-server-r2dbc/src/main/kotlin/pd/sensor/reactive/server/controller/SensorDataController.kt
+++ reactive-server-r2dbc/src/main/kotlin/pd/sensor/reactive/server/controller/SensorDataController.kt
@@ -3,15 +3,27 @@ package pd.sensor.reactive.server.controller
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.onStart
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Controller
 import pd.sensor.domain.SensorData
 import pd.sensor.reactive.server.service.SensorDataService
+import reactor.core.publisher.Hooks
+import javax.annotation.PostConstruct
 
 @Controller
 @MessageMapping("api.v2.sensors")
 class SensorDataController(val sensorDataService: SensorDataService) {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun init() {
+        Hooks.onErrorDropped {
+            log.warn("Error dropped: ${it.message}")
+        }
+    }
 
     @MessageMapping("stream")
     suspend fun write(@Payload inboundFlow: Flow<SensorData>) =
--- reactive-server-redis/src/main/kotlin/pd/sensor/reactive/server/controller/SensorDataController.kt
+++ reactive-server-redis/src/main/kotlin/pd/sensor/reactive/server/controller/SensorDataController.kt
@@ -4,15 +4,26 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.onStart
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Controller
 import pd.sensor.domain.SensorData
 import pd.sensor.reactive.server.service.SensorDataService
+import reactor.core.publisher.Hooks
+import javax.annotation.PostConstruct
 
 @Controller
 @MessageMapping("api.v2.sensors")
 class SensorDataController(val sensorDataService: SensorDataService) {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    fun init() {
+        Hooks.onErrorDropped {
+            log.warn("Error dropped: ${it.message}")
+        }
+    }
 
     @FlowPreview
     @MessageMapping("stream")
